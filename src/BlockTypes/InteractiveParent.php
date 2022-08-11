2906ac4d5027b12e3759e849a520471350405f10
David Arenas <david.arenas@automattic.com>
2022-08-11 13:58:07 +0200

    Create block types for bhe blocks

---
+++ src/BlockTypes/InteractiveParent.php
@@ -0,0 +1,21 @@
+<?php
+namespace Automattic\WooCommerce\Blocks\BlockTypes;
+
+/**
+ * InteractiveParent class.
+ */
+class InteractiveParent extends AbstractBlock {
+	/**
+	 * Block namespace.
+	 *
+	 * @var string
+	 */
+	protected $namespace = 'bhe';
+
+	/**
+	 * Block name.
+	 *
+	 * @var string
+	 */
+	protected $block_name = 'interactive-parent';
+}
---
+++ src/BlockTypes/NonInteractiveParent.php
@@ -0,0 +1,21 @@
+<?php
+namespace Automattic\WooCommerce\Blocks\BlockTypes;
+
+/**
+ * NonInteractiveParent class.
+ */
+class NonInteractiveParent extends AbstractBlock {
+	/**
+	 * Block namespace.
+	 *
+	 * @var string
+	 */
+	protected $namespace = 'bhe';
+
+	/**
+	 * Block name.
+	 *
+	 * @var string
+	 */
+	protected $block_name = 'non-interactive-parent';
+}
