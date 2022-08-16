d8bad910343db8770f6750dd5d855e2753363aa3
Carlos Bravo <carlos.bravo@automattic.com>
2022-08-16 16:02:54 +0200

    Remove not needed save js

--- assets/js/blocks/cart/inner-blocks/cart-order-summary-coupon-form/frontend.tsx
+++ assets/js/blocks/cart/inner-blocks/cart-order-summary-coupon-form/frontend.tsx
@@ -1,7 +1,28 @@
 /**
- * Internal dependencies
+ * External dependencies
  */
+import { TotalsCoupon } from '@woocommerce/base-components/cart-checkout';
+import { useStoreCartCoupons } from '@woocommerce/base-context/hooks';
+import { getSetting } from '@woocommerce/settings';
+import { TotalsWrapper } from '@woocommerce/blocks-checkout';
 
-import Block from './block';
+const Block = ( { className }: { className: string } ): JSX.Element | null => {
+	const couponsEnabled = getSetting( 'couponsEnabled', true );
+
+	const { applyCoupon, isApplyingCoupon } = useStoreCartCoupons( 'wc/cart' );
+
+	if ( ! couponsEnabled ) {
+		return null;
+	}
+
+	return (
+		<TotalsWrapper className={ className }>
+			<TotalsCoupon
+				onSubmit={ applyCoupon }
+				isLoading={ isApplyingCoupon }
+			/>
+		</TotalsWrapper>
+	);
+};
 
 export default Block;
